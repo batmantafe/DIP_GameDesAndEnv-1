aaf3067c919284a7ea6ec2dfbad2d4b77d46dc2c
Duane Reymond <duane.reymond@gmail.com>
2018-02-21 23:43:02 +1100

    Prefabs: Added Tunnel. Scripts: Added Movement (for Player) and Environment (for Tunnel prefab). Materials: Added Player and Tunnel materials.

---
+++ Assets/Scripts/Environment/Level.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float moveSpeed;
+
+    public GameObject[] obstaclesArray;
+    private int randomOb1, randomOb2, randomOb3, randomOb4;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        RandomObstacles();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void RandomObstacles()
+    {
+        if (!gameObject.CompareTag("Start"))
+        {
+            randomOb1 = Random.Range(0, 9);
+            randomOb2 = Random.Range(0, 9);
+            randomOb3 = Random.Range(0, 9);
+            randomOb4 = Random.Range(0, 9);
+
+            obstaclesArray[randomOb1].SetActive(true);
+            obstaclesArray[randomOb2].SetActive(true);
+            obstaclesArray[randomOb3].SetActive(true);
+            obstaclesArray[randomOb4].SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrust;
+
+    private bool keyPressed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        keyPressed = false;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W) && keyPressed == false)
+        {
+            keyPressed = true;
+            rb.AddForce(transform.up * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.S) && keyPressed == false)
+        {
+            keyPressed = true;
+            rb.AddForce(-transform.up * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.A) && keyPressed == false)
+        {
+            keyPressed = true;
+            rb.AddForce(-transform.right * thrust);
+        }
+
+        if (Input.GetKey(KeyCode.D) && keyPressed == false)
+        {
+            keyPressed = true;
+            rb.AddForce(transform.right * thrust);
+        }
+
+        else
+        {
+            keyPressed = false;
+        }
+    }
+}
