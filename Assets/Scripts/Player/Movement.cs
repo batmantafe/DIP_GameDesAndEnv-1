18bbc326283b5dca13d35de0fd80396b47d53091
Duane Reymond <duane.reymond@gmail.com>
2018-02-22 20:51:42 +1100

    Added working Lights to Player to assist range-finding. Actually works now when Player key inputs toggling the lights.

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -6,6 +6,8 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody rb;
     public float thrust;
+
+    public GameObject botLight, topLight, leftLight, rightLight;
     
     private bool keyPressed;
 
@@ -15,8 +17,6 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody>();
 
         keyPressed = false;
-
-        
     }
 
     // Update is called once per frame
@@ -27,28 +27,60 @@ public class Movement : MonoBehaviour
 
     void Move()
     {
+        // Player presses Up
         if (Input.GetKey(KeyCode.W) && keyPressed == false)
         {
             keyPressed = true;
             rb.AddForce(transform.up * thrust);
+
+            topLight.GetComponent<Light>().enabled = true;
         }
 
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            topLight.GetComponent<Light>().enabled = false;
+        }
+
+        // Player presses Down
         if (Input.GetKey(KeyCode.S) && keyPressed == false)
         {
             keyPressed = true;
             rb.AddForce(-transform.up * thrust);
+
+            botLight.GetComponent<Light>().enabled = true;
         }
 
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            botLight.GetComponent<Light>().enabled = false;
+        }
+
+        // Player presses Left
         if (Input.GetKey(KeyCode.A) && keyPressed == false)
         {
             keyPressed = true;
             rb.AddForce(-transform.right * thrust);
+
+            leftLight.GetComponent<Light>().enabled = true;
         }
 
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            leftLight.GetComponent<Light>().enabled = false;
+        }
+
+        // Player presses Right
         if (Input.GetKey(KeyCode.D) && keyPressed == false)
         {
             keyPressed = true;
             rb.AddForce(transform.right * thrust);
+
+            rightLight.GetComponent<Light>().enabled = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            rightLight.GetComponent<Light>().enabled = false;
         }
 
         else
