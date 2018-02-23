d2e00c28d4bbdab68a33d6dfcd8ebb1655d16380
Duane Reymond <duane.reymond@gmail.com>
2018-02-24 10:25:58 +1100

    Changed Red colour Hex. Added Gun and Bullet prefabs. Added Gun script (not working correctly yet).

---
+++ Assets/Scripts/Environment/Gun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletSpawn;
+    public float bulletSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        bulletSpeed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject clone = Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+
+        clone.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
+    }
+}
