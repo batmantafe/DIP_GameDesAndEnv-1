021f97e17154d7f91f52ca29fbe98bc0b9926670
Duane Reymond <duane.reymond@gmail.com>
2018-02-25 13:25:20 +1100

    Added Yellow Flash when Laser hits.

--- Assets/Scripts/Environment/BulletLife.cs
+++ Assets/Scripts/Environment/BulletLife.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class BulletLife : MonoBehaviour
 {
+    public GameObject bullet, bulletExplosion;
+    public float explosionTime;
+    
     // Use this for initialization
     void Start()
     {
-
+        explosionTime = 0.05f;
     }
 
     // Update is called once per frame
@@ -18,6 +21,16 @@ public class BulletLife : MonoBehaviour
 
     void OnTriggerEnter()
     {
+        StartCoroutine(LaserHit());
+    }
+
+    IEnumerator LaserHit()
+    {
+        bulletExplosion.SetActive(true);
+        bullet.SetActive(false);
+
+        yield return new WaitForSeconds(explosionTime);
+
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/Environment/LaserExplosion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserExplosion : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LaserHitTarget());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator LaserHitTarget()
+    {
+        // Nothing here.
+
+        yield return new WaitForSeconds(0.05f);
+
+        Destroy(gameObject);
+    }
+}
